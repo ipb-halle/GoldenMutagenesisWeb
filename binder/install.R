9465b9a33410423a7c0590838b542ca54f20d8ba
Chris Ulpinnis <culpinins@gmail.com>
2019-02-25 13:15:06 +0100

    Added Binder files

---
+++ binder/install.R
@@ -0,0 +1,16 @@
+## General dependencies:
+install.packages("seqinr")
+install.packages("stringr")
+install.packages("devtools")
+install.packages("shinyalert")
+install.packages("rlist")
+install.packages("RColorBrewer)
+## BioC dependencies
+
+#install.packages("BiocManager")
+#BiocManager::install(c("Biostrings", "sangerseqR"))
+
+source("https://bioconductor.org/biocLite.R")
+biocLite(c("Biostrings", "sangerseqR"))
+
+devtools::install_github("ipb-halle/GoldenMutagenesis", ref="prerelease-dev")
