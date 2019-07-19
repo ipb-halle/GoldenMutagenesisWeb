696c8f6f47931ff0c6a162185f708b717500a78f
culpinnis <culpinnis@gmail.com>
2019-07-19 19:20:53 +0200

    Updated dependencies

--- binder/install.R
+++ binder/install.R
@@ -7,9 +7,10 @@ install.packages("rlist")
 install.packages("RColorBrewer")
 install.packages("BiocManager")
 install.packages("plotly")
+install.packages("DT")
 ## BioC dependencies
 
 install.packages("BiocManager")
 BiocManager::install(c("Biostrings", "sangerseqR"))
-
+devtools::install_github('jbryer/DTedit')
 devtools::install_github("ipb-halle/GoldenMutagenesis")
